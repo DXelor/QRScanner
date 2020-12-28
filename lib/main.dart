be07ae1066a2b6eb4f646d8aa15a3606054f90df
DXelor <dzaliaspro@gmail.com>
2020-12-28 14:03:34 -0500

    cambiado el color y agregado un ThemeData en main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'QR Reader',
-        initialRoute: 'home',
-        routes: {'home': (_) => HomePage(), 'mapa': (_) => MapaPage()});
+      debugShowCheckedModeBanner: false,
+      title: 'QR Reader',
+      initialRoute: 'home',
+      routes: {'home': (_) => HomePage(), 'mapa': (_) => MapaPage()},
+      theme: ThemeData(
+          primaryColor: Colors.deepPurple,
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
+              backgroundColor: Colors.deepPurple)),
+    );
   }
 }
