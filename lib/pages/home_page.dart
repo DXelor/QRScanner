d194c1d8ae87c45c1dfe82a5599813ee7bf8aea8
DXelor <dzaliaspro@gmail.com>
2021-01-02 20:50:10 -0500

    agregado ui_provider.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
 import 'package:qr_reader/pages/direcciones_page.dart';
 import 'package:qr_reader/pages/mapas_page.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
 
 import 'package:qr_reader/widgets/custom_navigatorbar.dart';
 import 'package:qr_reader/widgets/scan_button.dart';
@@ -27,9 +30,12 @@ class HomePage extends StatelessWidget {
 class _HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final currentIndex = 1;
+    //obtener el selected menu opt
+    final uiProvider = Provider.of<UiProvider>(context);
 
     //Cambiar para mostrar la pagina respectiva
+    final currentIndex = uiProvider.selectedMenuOpt;
+
     switch (currentIndex) {
       case 0:
         return MapasPage();
