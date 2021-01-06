d7845fa614fee9379dae386d3f957a873a4f2f37
DXelor <dzaliaspro@gmail.com>
2021-01-05 23:10:04 -0500

    agregado el flutter_barcode_scanner

--- lib/pages/direcciones_page.dart
+++ lib/pages/direcciones_page.dart
@@ -4,10 +4,9 @@ class DireccionesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      elevation: 0,
-      actions: [IconButton(icon: Icon(Icons.delete_forever), onPressed: () {})],
-      title: Text('Direcciones'),
-    ));
+      body: Center(
+        child: Text('Direcciones Page'),
+      ),
+    );
   }
 }
