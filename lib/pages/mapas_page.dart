e6c53c96f9966b18fd0bdd8033a9c89569bfbe2d
DXelor <dzaliaspro@gmail.com>
2021-01-11 19:40:09 -0500

    Mostrar scans UI - Sin base de datos

--- lib/pages/mapas_page.dart
+++ lib/pages/mapas_page.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 class MapasPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Mapas Page'),
-      ),
-    );
+    return ListView.builder(
+        itemCount: 10,
+        itemBuilder: (/*BuildContext context*/ _, i) => ListTile(
+              leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
+              title: Text('http://sadw2q23'),
+              subtitle: Text('ID: 1'),
+              trailing: Icon(Icons.keyboard_arrow_right, color: Colors.amber),
+              onTap: () => print('abrir algo...'),
+            ));
   }
 }
