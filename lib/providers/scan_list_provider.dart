7298f06417d338b7a69042c048999bdff83ec224
DXelor <dzaliaspro@gmail.com>
2021-01-11 19:26:32 -0500

    leer Scans de la base de datos

--- lib/providers/scan_list_provider.dart
+++ lib/providers/scan_list_provider.dart
@@ -18,4 +18,30 @@ class ScanListProvider extends ChangeNotifier {
       notifyListeners();
     }
   }
+
+  cargarScans() async {
+    final scans = await DBProvider.db.getTodosLosScans();
+
+    this.scans = [...scans];
+    notifyListeners();
+  }
+
+  cargarScansByType(String tipo) async {
+    final scans = await DBProvider.db.getScansByType(tipo);
+    this.scans = [...scans];
+    this.tipoSeleccionado = tipo;
+    notifyListeners();
+  }
+
+  borrarTodos() async {
+    await DBProvider.db.deleteAllScans();
+    this.scans = [];
+    notifyListeners();
+  }
+
+  borrarScanPorId(int id) async {
+    await DBProvider.db.deleteScans(id);
+    this.cargarScansByType(this.tipoSeleccionado);
+    this.scans = [];
+  }
 }
